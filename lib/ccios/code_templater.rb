a750d16d5b0eff3167af1449a3eec7632ce296ff
Pierre Felgines <pierre.felgines@applidium.com>
2017-09-04 16:53:15 +0200

    Fix missing option

--- lib/ccios/code_templater.rb
+++ lib/ccios/code_templater.rb
@@ -7,7 +7,7 @@ class CodeTemplater
 
   def content_for_suffix(prefix, suffix)
     template_name = suffix.underscore
-    options = @options.merge({name: prefix})
+    options = @options.merge({name: prefix, lowercased_name: prefix.camelize(:lower)})
     template_file = File.join(File.dirname(__FILE__), "templates/#{template_name}.mustache")
     Mustache.render(File.read(template_file), options)
   end
