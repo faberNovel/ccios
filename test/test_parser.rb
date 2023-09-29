c965bc4b56df87db75126d9de4069a6cc742639c
Edouard Siegel <edouard.siegel@gmail.com>
2023-09-29 09:16:01 +0200

    Remove warnings silencing

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -5,25 +5,21 @@ require 'xcodeproj'
 class ParserTest < Minitest::Test
 
   def test_projects_are_equal
-    Kernel.silence_warnings do # xcodeproj emit lots of warnings during `rake test` and not in irb
-      dir = File.join(File.dirname(__FILE__), "project")
-      config = Config.default
-      parser = PBXProjParser.new(dir, config)
+    dir = File.join(File.dirname(__FILE__), "project")
+    config = Config.default
+    parser = PBXProjParser.new(dir, config)
 
-      assert parser.app_project.equal? parser.core_project
-      assert parser.app_project.equal? parser.data_project
-    end
+    assert parser.app_project.equal? parser.core_project
+    assert parser.app_project.equal? parser.data_project
   end
 
   def test_targets_are_equal
-    Kernel.silence_warnings do # xcodeproj emit lots of warnings during `rake test` and not in irb
-      dir = File.join(File.dirname(__FILE__), "project")
-      config = Config.default
-      parser = PBXProjParser.new(dir, config)
+    dir = File.join(File.dirname(__FILE__), "project")
+    config = Config.default
+    parser = PBXProjParser.new(dir, config)
 
-      assert parser.app_target.equal? parser.core_target
-      assert parser.app_target.equal? parser.data_target
-    end
+    assert parser.app_target.equal? parser.core_target
+    assert parser.app_target.equal? parser.data_target
   end
 
 end
