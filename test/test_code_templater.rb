8c224e8ede685db139e01fd20726993408a4227c
Alexandre Podlewski <alexandre.podlewski@fabernovel.com>
2024-02-20 16:47:18 +0100

    Update test for CodeTemplater

--- test/test_code_templater.rb
+++ test/test_code_templater.rb
@@ -2,42 +2,81 @@ require 'minitest/autorun'
 require_relative '../lib/ccios/code_templater'
 
 class CodeTemplaterTest < Minitest::Test
-  def test_all_templates
-    templates_files.each do |template_path|
-      basename = basename_for_path template_path
-      generate_delegate = !basename.slice!("_delegate").nil? # Removes _delegate from basename as well
 
-      templates_dir = Config.default.templates.path
-      templater = CodeTemplater.new(options(generate_delegate), templates_dir)
-      template_content = templater.content_for_suffix("Test", basename)
-      assert_equal_content_of_file(template_path, template_content)
+  def test_string_rendering
+    result = CodeTemplater.new.render_string(
+      string_template_with_one_parameter,
+      {value: "42"}
+    )
+    assert_equal result, "String with parameter: 42."
+
+    result = CodeTemplater.new.render_string(
+      string_template_with_multiple_parameter,
+      {param1: "param1", param2: "param2"}
+    )
+    assert_equal result, "String with parameters: param1, param2, param2."
+  end
+
+  def test_get_string_template_variables
+    result = CodeTemplater.new.get_unknown_context_keys_for_string(string_template_with_one_parameter)
+    assert_equal result, ["value"].to_set
+
+    result = CodeTemplater.new.get_unknown_context_keys_for_string(string_template_with_multiple_parameter)
+    assert_equal result, ["param1", "param2"].to_set
+  end
+
+  def test_render_file_content_from_template
+    Tempfile.create do |f|
+      f << test_template_content
+      f.rewind
+      result = CodeTemplater.new.render_file_content_from_template(
+        f.path,
+        "Sample.swift",
+        {
+          name: "Sample",
+          project_name: "ProjectName",
+          full_username: "Georges Abitbol",
+          date: "01/01/1970",
+          generate_delegate: true
+        }
+      )
+      assert_equal result, test_template_content_expected_result
     end
   end
 
-  # Private
+  private
 
-  def templates_files
-    templates_path = File.join(File.dirname(__FILE__), "templates")
-    Dir[templates_path + "/*"]
+  def string_template_with_one_parameter
+    "String with parameter: {{ value }}."
   end
 
-  def basename_for_path(path)
-    ext = File.extname path
-    File.basename path, ext
+  def string_template_with_multiple_parameter
+    "String with parameters: {{ param1 }}, {{ param2 }}, {{ param2 }}."
   end
 
-  def assert_equal_content_of_file(path, template)
-    file = File.open(path, "rb")
-    correct_template = file.read
-    assert_equal template, correct_template
+  def test_template_content
+    <<-eos
+//  {{ name }}.swift
+//  {{project_name}}
+//  Created by {{full_username}} on {{date}}.
+
+{{#generate_delegate}}
+// Generated with delegates
+
+{{/generate_delegate}}
+class {{ name }} {}
+eos
   end
 
-  def options(generate_delegate)
-    {
-      project_name: "ProjectName",
-      full_username: "Georges Abitbol",
-      date: "01/01/1970",
-      generate_delegate: generate_delegate,
-    }
+  def test_template_content_expected_result
+    <<-eos
+//  Sample.swift
+//  ProjectName
+//  Created by Georges Abitbol on 01/01/1970.
+
+// Generated with delegates
+
+class Sample {}
+eos
   end
 end
